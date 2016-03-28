665327c125650a9b92ace2e12a64dd3a1a854b94
Jef Van den broeck <jef@codewerken.be>
2016-03-28 22:33:11 +0200

    reinstated company info, total price, modal dialog with overview

--- src/fr1ckets/static/js/fr1ckets.js
+++ src/fr1ckets/static/js/fr1ckets.js
@@ -1,4 +1,60 @@
 var products = [];
+var discount = 0;
+
+$('#ticket_form').submit(function(e) {
+	e.preventDefault();
+	$('#overview_content').html(update_overview());
+	$('#overview_modal').modal('show');
+	$('#overview_order').on('click', function() {
+		console.log("calling");
+		$.ajax({
+			url : window.location.href,
+			type : 'post',
+			data : $('form#ticket_form').serialize(),
+		});
+		console.log($('form#ticket_form').serialize());
+	});
+});
+
+function update_overview() {
+
+	var choices = enumerate_choices();
+	var f = '';
+
+	f += '<table class="table">';
+	f += '  <thead>';
+	f += '    <tr>';
+	f += '      <th>Wat</th>';
+	f += '      <th>Hoeveel</th>';
+	f += '      <th>Prijs</th>';
+	f += '    </tr>';
+	f += '  </thead>';
+	f += '  <tbody>';
+	for (var i = 0; i < choices.length; i++) {
+		f += '    <tr>';
+		f += '      <td>'+choices[i].name+'</td>';
+		f += '      <td>'+choices[i].n+'</td>';
+		f += '      <td>'+choices[i].price+'</td>';
+		f += '    </tr>';
+	}
+	if (discount) {
+		f += '    <tr class="success">';
+		f += '      <td>Korting</td>';
+		f += '      <td></td>';
+		f += '      <td>€'+discount+'</td>';
+		f += '    </tr>';
+	}
+	f += '    <tr>';
+	f += '      <td><strong>Totaal</strong></td>';
+	f += '      <td></td>';
+	f += '      <td><strong>€'+calculate_total()+'</strong></td>';
+	f += '    </tr>';
+	f += '  </tbody>';
+	f += '</table>';
+
+	return f;
+
+}
 
 function handle_reservation(data) {
 
@@ -27,6 +83,8 @@ function handle_reservation(data) {
 		f += '</div>';
 	}
 
+	discount = reservation.discount;
+
 	$("#email_reservation_collapse").html(f);
 
 }
@@ -46,7 +104,6 @@ function find_ticket_by_dob(dob, billable) {
 			break;
 		}
 	}
-	console.log("mapped date "+dob+" to ticket "+ticket.name);
 	return ticket;
 
 }
@@ -79,25 +136,55 @@ function enumerate_choices() {
 		var month_src = src + '_dob_month';
 		var day_src = src + '_dob_day';
 		var name_src = src + '_name';
-		var premium_src = src + '_options_premium';
-		var cleanup_src = src + '_options_cleanup_toggle';
+		var volunteering_src = src + "_options_volunteers_during";
+		var cleanup_src = src + "_options_volunteers_after";
+		var billable_src = src + "_billable";
+
 		var dob = new Date($('#'+year_src).val(), $('#'+month_src).val(), $('#'+day_src).val()).getTime();
-		var ticket = find_ticket_by_dob(dob, false);
+		var billable = Boolean($('#'+billable_src).prop('checked'));
+		var ticket = find_ticket_by_dob(dob, billable);
 		var name = $('#' + name_src).val();
-		var volunteering = !$('#'+premium_src).prop('checked') || $('#'+cleanup_src).prop('checked');
-		var price = ticket.price;
-		if (volunteering) {
-			price = ticket.volunteering_price;
+		var volunteering_during = Boolean($('#'+volunteering_src).prop('checked'))
+		var volunteering_after = Boolean($('#'+cleanup_src).prop('checked'));
+		var volunteering = volunteering_after || volunteering_during;
+		var ticket_price = ticket.volunteering_price;
+		var ticket_name = ticket.display + " voor " + name;
+		var can_volunteer = Boolean(dob < ticket_volunteering_cutoff);
+		if (can_volunteer && !billable && !volunteering) {
+			ticket_price = ticket.price;
+			ticket_name = ticket_name + " (premium)";
 		}
 		var c = {
 			n : 1,
-			price : price,
-			name : ticket.display + " voor " + name,
+			price : ticket_price,
+			name : ticket_name,
 		}
 		choices.push(c);
 	}
 
-	console.dir(choices);
+	return choices;
+
+}
+
+function calculate_total() {
+
+	var choices = enumerate_choices();
+	var total = 0;
+
+	for (var i = 0; i < choices.length; i++) {
+		console.dir(choices[i]);
+		total += choices[i].n * choices[i].price;
+	}
+
+	total = Math.max(0, total - discount);
+
+	return total;
+
+}
+
+function update_price_total_display() {
+
+	$("#price_total").html('€'+calculate_total());
 
 }
 
@@ -110,6 +197,7 @@ $(document).ready(function() {
 				if (products[i].max_dob) {
 					products[i].max_dob *= 1000;
 				}
+				$('#'+products[i].name).on('change', update_price_total_display);
 			}
 		},
 	});
@@ -128,83 +216,11 @@ $(document).ready(function() {
 			handle_reservation('');
 		}
 	});
+	$('#n_tickets').on('change', update_price_total_display);
+	update_price_total_display();
 });
 
-function recalc_total() {
-	var total = 0;
-	for (thing in prices) {
-		var n = parseInt($('#'+thing).val());
-		total += n * prices[thing];
-	}
-	$('#price_total').html("€" + total);
-}
-
-function mark_paid(id) {
-	$.ajax({
-		url: "api/purchase_mark_paid/" + id,
-		success: function(data) {
-			$('#mark_paid_' + id).html("great!");
-			$('#remove_' + id).html("great!");
-		},
-		error: function(data) {
-			$('#mark_paid_' + id).html('FAILED!');
-		},
-	});
-}
-
-var tickets_normal = [
-	{
-		name : '3bit ticket',
-		price : 8,
-		premium : 0,
-		dob : Date.now() - 3 * 3600*24*356*1000,
-	},
-	{
-		name : '4bit ticket',
-		price : 16,
-		premium : 0,
-		dob : Date.now() - 6 * 3600*24*356*1000,
-	},
-	{
-		name : '5bit ticket',
-		price : 32,
-		premium : 0,
-		dob : Date.now() - 12 * 3600*24*356*1000,
-	},
-	{
-		name : '6bit ticket',
-		price : 64,
-		premium : 8,
-		dob : Date.now() - 24 * 3600*24*356*1000,
-	},
-	{
-		name : '7bit ticket',
-		price : 128,
-		premium : 16,
-		dob : Date.now() - 1000 * 3600*24*356*1000,
-	},
-];
-var tickets_billable = [
-	{
-		name : '8bit ticket',
-		price : 256,
-		premium : 0,
-		dob : Date.now() - 1000 * 3600*24*356*1000,
-	},
-];
-
 var ticket_volunteering_cutoff = Date.now() - 16 * 3600*24*356*1000;
-function purchase_remove(id) {
-	$.ajax({
-		url: "api/purchase_remove/" + id,
-		success: function(data) {
-			$('#remove_' + id).html("great!");
-		},
-		error: function(data) {
-			$('#remove_' + id).html('FAILED!');
-		},
-	});
-}
 
 var showing_business_info = false;
 function display_business_info() {
@@ -212,7 +228,6 @@ function display_business_info() {
 	var n_tickets = parseInt($('#n_tickets').val());
 	var n_tickets_billable = 0;
 
-	console.log("checking");
 	for (var i = 0; i < n_tickets; i++) {
 		var fmt = "tickets_"+i;
 		var billable_id = fmt+"_billable";
@@ -293,7 +308,7 @@ function mk_cb_update_visitor_options(index) {
 			f += '    <p id="'+ticket_name_id+'">'+ticket.display+'</p>';
 			f += '  </div>';
 			f += '  <div class="col-sm-2 text-right">';
-			f += '    <p id="'+ticket_price_id+'">€'+ticket.price+'</p>';
+			f += '    <p id="'+ticket_price_id+'">€'+ticket.volunteering_price+'</p>';
 			f += '  </div>';
 			f += '</div>';
 			f += '<div class="form-group">';
@@ -304,28 +319,17 @@ function mk_cb_update_visitor_options(index) {
 			f += '    </label>';
 			f += '  </div>';
 		}
-		if (billable && can_volunteer) {
+		if (can_volunteer) {
 			var volunteering_id = fmt + "_options_volunteers_during";
 			var cleanup_id = fmt + "_options_volunteers_after";
 			f += '  <div class="checkbox col-sm-4 col-xs-6">';
 			f += '    <label>';
-			f += '      <input type="checkbox" id="'+volunteering_id+'" name="'+volunteering_id+'">';
+			if (billable) {
+				f += '      <input type="checkbox" id="'+volunteering_id+'" name="'+volunteering_id+'">';
+			} else {
+				f += '      <input type="checkbox" id="'+volunteering_id+'" name="'+volunteering_id+'" checked="checked">';
+			}
 			f += '      Help graag mee tijdens kamp';
-			f += '    </label>';
-			f += '  </div>';
-			f += '  <div class="checkbox col-sm-offset-4 col-sm-8 col-xs-6">';
-			f += '    <label>';
-			f += '      <input type="checkbox" id="'+cleanup_id+'" name="'+cleanup_id+'">';
-			f += '      Help opkuisen de dag er na (pizza voorzien!)';
-			f += '    </label>';
-			f += '  </div>';
-		} else if (!billable && can_volunteer) {
-			var premium_id = fmt + "_options_premium";
-			var cleanup_id = fmt + "_options_volunteers_after";
-			f += '  <div class="checkbox col-sm-4 col-xs-6">';
-			f += '    <label>';
-			f += '      <input type="checkbox" id="'+premium_id+'" name="'+premium_id+'">';
-			f += '      Premium';
 			f += '    </label>';
 			f += '  </div>';
 			f += '  <div class="checkbox col-sm-offset-4 col-sm-8 col-xs-6">';
@@ -338,45 +342,41 @@ function mk_cb_update_visitor_options(index) {
 		f += '</div>';
 		// throw it into the DOM so we can add events to it
 		$('#'+options_id).html(f);
-		if (billable && can_volunteer) {
-			;
-		} else if (!billable && can_volunteer) {
-			var premium_id = fmt + "_options_premium";
-			var cleanup_id = fmt + "_options_volunteers_after";
-			$('#'+premium_id).on('change', function() {
-				var display_name = '';
-				var display_price = 0;
-				var is_premium = Boolean(this.checked);
-				if (is_premium) {
-					display_name = ticket.display + ' (premium)';
-					display_price = ticket.price;
-				} else {
-					display_name = ticket.display;
-					display_price = ticket.volunteering_price;
-				}
-				$('#'+ticket_name_id).text(display_name);
-				$('#'+ticket_price_id).text('€'+display_price);
-			});
+		if (can_volunteer) {
+			if (!billable) {
+				var volunteering_id = fmt + "_options_volunteers_during";
+				var cleanup_id = fmt + "_options_volunteers_after";
+				$('#'+volunteering_id).on('change', function() {
+					var display_name = '';
+					var display_price = 0;
+					var is_volunteering = Boolean(this.checked);
+					if (!is_volunteering) {
+						display_name = ticket.display + ' (premium)';
+						display_price = ticket.price;
+					} else {
+						display_name = ticket.display;
+						display_price = ticket.volunteering_price;
+					}
+					/*
+					$('#'+ticket_name_id).fadeOut(100, function() {
+						$(this).text(display_name).fadeIn(100);
+					});
+					$('#'+ticket_price_id).fadeOut(100, function() {
+						$(this).text('€'+display_price).fadeIn(100);
+					});
+					*/
+					$('#'+ticket_name_id).text(display_name);
+					$('#'+ticket_price_id).text('€'+display_price);
+					update_price_total_display();
+				});
+			}
 		}
 
-		/*
-		update_ticket(ticket, false);
-
-		// wire in needed handlers
-		$('#'+visitor_premium_toggle).on('change', function() {
-			var premium = this.checked;
-			if (premium) {
-				update_ticket(ticket, true);
-				$('#'+visitor_cleanup_toggle).prop('checked', false);
-				$('#'+visitor_cleanup_toggle).prop('disabled', true);
-			} else {
-				update_ticket(ticket, false);
-				$('#'+visitor_cleanup_toggle).prop('disabled', false);
-			}
-		});
-		*/
 		// and collapse it
 		$('#'+options_id).collapse('show');
+
+		// update main total
+		update_price_total_display();
 
 	}
 }
