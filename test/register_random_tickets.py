9bf80678a103a8ab1cc3bc89fc1f44cb04111650
Jef Van den broeck <jef@codewerken.be>
2016-05-21 18:19:06 +0200

    multiprocess tester

--- test/register_random_tickets.py
+++ test/register_random_tickets.py
@@ -7,12 +7,15 @@ import random
 import string
 import time
 import datetime
+import json
+import multiprocessing
+
+auth=('hello', 'world')
+url = sys.argv[1]
+n_concurrent = int(sys.argv[2])
 
 def D(*args, **kwargs):
 	print("{0} {1}".format(pprint.pformat(*args), pprint.pformat(kwargs) if kwargs else ''))
-
-auth=('hello', 'world')
-url=sys.argv[1]
 
 class Form(object):
 	tshirt_choices = [ 'tshirt_adult_m_' + size for size in [ 'xs', 's', 'm', 'l', 'xl', 'xxl' ] ]
@@ -21,7 +24,6 @@ class Form(object):
 	token_choices = [ 0, 5, 10, 15, 20, 30, 40, 50, 100 ]
 
 	def __init__(self):
-		print self.tshirt_choices
 		self.data = {}
 		self.n_tickets = 0
 		for k in [ 'payment', 'supervision', 'excellent' ]:
@@ -106,33 +108,84 @@ class RandomForm(Form):
 					base + 'street 1\n' + base + 'ville\n' + base + 'land',
 					'BTW 111.111.4444')
 
-s = requests.session()
+def order_random_tickets(queue, url, min_tickets, max_tickets):
+	s = requests.session()
+	
+	ret = {}
+	ret['error_data'] = []
 
-ts = time.time()
-
-r = s.get(url+'/tickets', auth=auth)
-
-timing_page = time.time() - ts
-
-page = html.fromstring(r.text)
-csrf_token = page.forms[0].fields['csrf_token']
-r = RandomForm()
-r.fill(random.randrange(1, 5), csrf_token)
-
-ts = time.time()
-x = s.get(url+'/api/get_reservation/' + r.data['email'], auth=auth)
-timing_email = time.time() - ts
-
-ts = time.time()
-o = s.post(url+'/api/tickets_register', auth=auth, data=r.data)
-timing_post = time.time() - ts
-import json
-do = json.loads(o.text)
-if 'redirect' in do:
+	# get the ticket page
 	ts = time.time()
-	o = s.get(url+do['redirect'], auth=auth)
-	timing_confirm = time.time() - ts
-else:
-	timing_confirm = 0
+	r = s.get(url+'/tickets', auth=auth)
+	ret['tickets_page'] = 'ERROR' if r.status_code != 200 else time.time() - ts
 
-print "timing: page={0} email={3} post={1} confirm={2} total={4}".format(timing_page, timing_post, timing_confirm, timing_email, timing_page+timing_post+timing_confirm+timing_email)
+	# parse the page, we need the CSRF token
+	try:
+		page = html.fromstring(r.text)
+		csrf_token = page.forms[0].fields['csrf_token']
+	except:
+		ret['tickets_page'] = 'ERROR'
+		ret['error_data'].append(r.text)
+		csrf_token = 'foo'
+
+	# generate tickets
+	form = RandomForm()
+	form.fill(random.randrange(min_tickets, max_tickets+1), csrf_token)
+
+	# check the reservation on email
+	ts = time.time()
+	r = s.get(url+'/api/get_reservation/' + form.data['email'], auth=auth)
+	ret['reservation'] = 'ERROR' if r.status_code != 200 else time.time() - ts
+
+	# actually order the tickets
+	ts = time.time()
+	r = s.post(url+'/api/tickets_register', auth=auth, data=form.data)
+	ret['registration'] = 'ERROR' if r.status_code != 200 else time.time() - ts
+
+	# check the JSON outcome
+	try:
+		registration_result = json.loads(r.text)
+	except:
+		ret['registration'] = 'ERROR'
+		registration_result = []
+		ret['error_data'].append(r.text)
+
+	if 'redirect' in registration_result:
+		ts = time.time()
+		r = s.get(url+registration_result['redirect'], auth=auth)
+		ret['confirmation'] = 'ERROR' if r.status_code != 200 else time.time() - ts
+	else:
+		ret['registration'] = 'ERROR'
+		ret['confirmation'] = 'ERROR'
+		ret['error_data'].append(registration_result)
+
+	if not 'ERROR' in ret.values():
+		ret['total'] = sum([ ret[k] for k in ret.keys() if 'data' not in k])
+
+	ret['data'] = form.data
+
+	queue.put(ret)
+
+start = time.time()
+processes = []
+for i in range(n_concurrent):
+	q = multiprocessing.Queue()
+	p = multiprocessing.Process(target=order_random_tickets, args=(q,url,1,3))
+	processes.append((p, q))
+	p.start()
+
+outcomes = []
+for p, q in processes:
+	outcomes.append(q.get())
+	p.join()
+
+n_errors = len([ o for o in outcomes if 'ERROR' in o.values() ])
+totals = [ o['total'] for o in outcomes if 'total' in o ]
+avg_total = sum(totals)/len(totals)
+for o in outcomes:
+	if not 'total' in o:
+		pprint.pprint("error:")
+		pprint.pprint(o)
+
+print "{0} calls completed on {1}, n_errors={2} avg_total={3}".format(len(outcomes),
+		time.time()-start, n_errors, avg_total)
