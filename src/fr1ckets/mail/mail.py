51d446572c5915d3fdcf1faa58cb4bbf3c170a13
Jef Van den broeck <jef@codewerken.be>
2016-05-31 21:36:37 +0200

    lose some debuggin

--- src/fr1ckets/mail/mail.py
+++ src/fr1ckets/mail/mail.py
@@ -5,8 +5,6 @@ from fr1ckets.background.tasks import mail
 from fr1ckets.background.tasks import notif
 
 def send_mail(from_addr, to_addrs, subject, msg_html, msg_text):
-	app.logger.debug(mail)
-	app.logger.debug(mail.name)
 	mail.delay(from_addr, to_addrs, subject, msg_html, msg_text,
 			(app.config['SMTP_USERNAME'], app.config['SMTP_PASSWORD']),
 			(app.config['SMTP_SERVER'], app.config['SMTP_PORT']))
