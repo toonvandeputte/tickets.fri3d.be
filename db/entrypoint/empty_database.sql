9c00d64369b46f78bc7fb0dfc409fae2b84c522d
Jef Van den broeck <jef@codewerken.be>
2018-04-28 18:53:10 +0200

    name tickets tickets instead of the bit patterns

--- db/entrypoint/empty_database.sql
+++ db/entrypoint/empty_database.sql
@@ -14,12 +14,12 @@ create table product (
 );
 
 insert into product (name, display, price, volunteering_price, max_dob, billable) values
-	( 'ticket_3bit',     '3bit 0-3 jaar',                  7, 7, '2015-08-18 02:00:00', 0),
-	( 'ticket_4bit',     '4bit 3-6 jaar',                  17, 17, '2012-08-18 02:00:00', 0),
-	( 'ticket_5bit',     '5bit 6-12 jaar',                 37, 37, '2006-08-18 02:00:00', 0),
-	( 'ticket_6bit',     '6bit 12-24 jaar',                73, 73, '1994-08-18 02:00:00', 0),
-	( 'ticket_7bit',     '7bit +24 jaar',                  157, 137, '1900-01-01 00:00:00', 0),
-	( 'ticket_8bit',     '8bit zakelijk',                  317, 317, '1900-01-01 00:00:00', 1),
+	( 'ticket_3bit',     'ticket 0-3 jaar',                  7, 7, '2015-08-18 02:00:00', 0),
+	( 'ticket_4bit',     'ticket 3-6 jaar',                  17, 17, '2012-08-18 02:00:00', 0),
+	( 'ticket_5bit',     'ticket 6-12 jaar',                 37, 37, '2006-08-18 02:00:00', 0),
+	( 'ticket_6bit',     'ticket 12-24 jaar',                73, 73, '1994-08-18 02:00:00', 0),
+	( 'ticket_7bit',     'ticket +24 jaar',                  157, 137, '1900-01-01 00:00:00', 0),
+	( 'ticket_8bit',     'ticket zakelijk',                  317, 317, '1900-01-01 00:00:00', 1),
 	( 'tshirt_adult_m_xs',  'volwassenen mannen tshirt XS',       20, 20, '1900-01-01 00:00:00', 0),
 	( 'tshirt_adult_m_s',  'volwassenen mannen tshirt S',       20, 20, '1900-01-01 00:00:00', 0),
 	( 'tshirt_adult_m_m',  'volwassenen mannen tshirt M',      20, 20, '1900-01-01 00:00:00', 0),
